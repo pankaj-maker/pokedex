557ab4a0eaa0b1202c7ecb2040f268a1da8767f0
pankaj-maker <Pankaj14321@protonmail.com>
2024-10-03 08:06:05 +0000

    search done

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
 import data from "./data";
 import Shuffle from "array-shuffle";
 import PokemonCard from "./components/PokemonCard";
+const inputEl=document.querySelector("input");
+const dataRow = document.querySelector("[data-row]");
 
 
 // === DOM Selection ===
@@ -8,8 +10,23 @@ renderPokemon(Shuffle(data))
 
 
 function renderPokemon(list){
+dataRow.textContent=""
     list.forEach((pokemonObj)=>{
         PokemonCard(pokemonObj);
     })
 }
+function handleSearch(input){
+    const filterPokemon=data.filter((pokemonObj))
+    pokemonObj.name.toLowerCase().includes(input)
+}
+inputEl.addEventListener("input",(e)=>{
+const cuurInput=e.target.value.trim().toLowerCase();
+    handleSearch(cuurInput)
+})
 
+document.addEventListener("keyup",(e)=>{
+if(e.key==="/"){
+    e.preventDefault();
+inputEl.focus();
+}
+});
