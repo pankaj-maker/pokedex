f2c5ecb8eb23df163954601edca44d6f3b960dfd
pankaj-maker <Pankaj14321@protonmail.com>
2024-10-08 08:11:44 +0000

    debunced

--- src/js/components/PokemonCard.js
+++ src/js/components/PokemonCard.js
@@ -1,21 +1,22 @@
-// const dataRow = document.querySelector("[data-row]");
-
-export default function PokemonCard(data){
-    const{image,link,description,name}=data;
+// This generates a card component from the data
+export default function (data) {
+    const { image, link, description, name } = data;
+  
     const div = document.createElement("div");
     div.classList.add("col");
-    div.innerHTML = `<div class="card">
-        <img
-            src="${image}"
-            class="card-img-top"
-            alt="${name}"
-        />
-        <div class="card-body">
-            <h5 class="card-title">${name}</h5>
-            <p class="card-text">${description}</p>
-            <a href="${link}" class="btn btn-warning">Visit</a>
-        </div>
-        </div>`;
-    // dataRow.appendChild(div);
+    div.innerHTML = `
+      <div class="card">
+          <img
+              src="${image}"
+              class="card-img-top"
+              alt="${name}"
+          />
+          <div class="card-body">
+              <h5 class="card-title">${name}</h5>
+              <p class="card-text">${description}</p>
+              <a href="${link}" class="btn btn-warning">Visit</a>
+          </div>
+      </div>
+    `;
     return div;
   }
--- src/js/main.js
+++ src/js/main.js
@@ -1,35 +1,79 @@
+// Packages
+import shuffle from "array-shuffle";
+import Fuse from "fuse.js";
+
 import data from "./data";
-import Shuffle from "array-shuffle";
 import PokemonCard from "./components/PokemonCard";
 
-//== DOM Targeting ===
-const inputEl=document.querySelector("input");
+// === DOM Targeting ===
+const inputEl = document.querySelector('input[type="text"]');
 const dataRow = document.querySelector("[data-row]");
 
+renderPokemon(shuffle(data));
 
-// === DOM Selection ===
-renderPokemon(Shuffle(data))
+// Give list, it will render them
+function renderPokemon(list) {
+  dataRow.textContent = "";
 
-
-function renderPokemon(list){
-dataRow.textContent=""
-    list.forEach((pokemonObj)=>{
-    const pokemon=PokemonCard(pokemonObj)
-        dataRow.appendChild(pokemon);
-    })
+  if(!list.length){
+  const pokemon=PokemonCard({
+    image:"https://upload.wikimedia.org/wikipedia/en/e/e4/Ash_Ketchum_Journeys.png?20240410031759",name:"Not found",link:"https://pokedex.com",description:"Try another search"
+  });
+  dataRow.appendChild(pokemon);
 }
-function handleSearch(input){
-    const filterPokemon=data.filter((pokemonObj))
-    pokemonObj.name.toLowerCase().includes(input)
-}
-inputEl.addEventListener("input",(e)=>{
-const cuurInput=e.target.value.trim().toLowerCase();
-    handleSearch(cuurInput)
-})
 
-document.addEventListener("keyup",(e)=>{
-if(e.key==="/"){
+  list.forEach((pokemonObj) => {
+    const pokemon = PokemonCard(pokemonObj);
+    dataRow.appendChild(pokemon);
+  });
+}
+
+// Will be invoked on search
+function handleSearch(input) {
+  // const filteredPokemon = data.filter((pokemonObj) => {
+  //   return pokemonObj.name.toLowerCase().includes(input);
+  // });
+
+  let debounceTimer;
+  inputEl.addEventListener("input",(e)=>{
+    clearTimeout(debounceTimer);
+    debounceTimer=setTimeout(()=>{
+const currentInput=e.target.value.trim().toLowerCase();
+    handleSearch(currentInput);
+
+  },500);
+});
+
+  // Create fuse object
+  const options = {
+    keys: ["name", "description"],
+    threshold: 0.5,
+  };
+  const fuse = new Fuse(data, options);
+
+  // Perform search
+  function performSearch() {
+    if (!input) return data;
+
+    const searched = fuse.search(input);
+    return searched.map((obj) => obj.item);
+  }
+
+  // Create without the 'item' key from fuse search
+  const filterdPokemon = performSearch();
+  renderPokemon(filterdPokemon);
+}
+
+inputEl.addEventListener("input", (e) => {
+  const currentInput = e.target.value.trim().toLowerCase();
+  handleSearch(currentInput);
+});
+
+// Add / to active search
+document.addEventListener("keydown", (e) => {
+  if (e.key === "/") {
+    // Don't type
     e.preventDefault();
-inputEl.focus();
-}
+    inputEl.focus();
+  }
 });
